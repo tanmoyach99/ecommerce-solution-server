89ac2dd33f6eb982feaab79469206b53ca76e399
tanmoyach99e <tanmoyacharjee18@gmail.comm>
2023-01-10 02:36:29 +0600

    ggg

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 console.log(process.env.port);
 
-const port=process.env.PORT|| 8000;
+const port = process.env.PORT || 8000;
 // app.get("/", (req, res) => {
 //   res.send("app is working");
 // });
